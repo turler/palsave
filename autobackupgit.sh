29c05cdc5c5a694a3b6297cc5ab21587ee27d50b
mint <flamintdao@gmail.com>
2024-04-06 11:41:07 +0000

    update script

--- autobackupgit.sh
+++ autobackupgit.sh
@@ -2,5 +2,6 @@
 
 docker exec palworld-server rcon-cli "save"
 
+cd ~ && rm -rf ~/palsave && git clone --filter=tree:0 git@github.com:turler/palsave.git
 cp -rf ~/palworld/Pal/Saved/SaveGames ~/palsave/ 
-git add . && git commit -m "Update auto" && git push origin main
+cd ~/palsave && git add . && git commit -m "Update auto" && git push origin main
